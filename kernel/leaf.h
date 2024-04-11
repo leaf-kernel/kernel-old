868eb52c68c17994fe5355fbb2c478ed0a61a0a8
Kevin Alavik <kevin@alavik.se>
2024-04-11 12:19:50 +0000

    Added return types

---
+++ kernel/leaf.h
@@ -0,0 +1,9 @@
+#ifndef __LEAF_H__
+#define __LEAF_H__
+
+// Leaf return codes.
+#define LEAF_RETURN_SUCCESS     0
+#define LEAF_RETURN_FAIL        1
+#define LEAF_RETURN_FATAL       2
+
+#endif // __LEAF_H__
