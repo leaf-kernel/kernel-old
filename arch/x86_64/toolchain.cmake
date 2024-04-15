85f900d42c946df0168d9e919212ad0704929888
Kevin Alavik <kevin@alavik.se>
2024-04-15 13:32:52 +0200

    cmake my ass

--- arch/x86_64/toolchain.cmake
+++ arch/x86_64/toolchain.cmake
@@ -1,7 +1,6 @@
 set(CMAKE_C_COMPILER   x86_64-elf-gcc)
 set(CMAKE_ASM_COMPILER nasm)
 
-# Check for x86_64-elf-gcc and nasm
 find_program(CMAKE_C_COMPILER_TARGET x86_64-elf-gcc)
 if(NOT CMAKE_C_COMPILER_TARGET)
     message(FATAL_ERROR "You need x86_64-elf-gcc to build Leaf for x86_64!")
@@ -12,10 +11,6 @@ if(NOT CMAKE_ASM_COMPILER_TARGET)
     message(FATAL_ERROR "You need nasm to assemble Leaf for x86_64!")
 endif()
 
-# Set compiler flags
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -O2 -pipe -O0 -Wall -Wextra -Werror -std=gnu11 -ffreestanding -fno-stack-protector -fno-stack-check -fno-lto -fno-PIE -fno-PIC -m64 -march=x86-64 -mabi=sysv -mcmodel=kernel -mno-80387 -mno-red-zone -msse -msse2 -Wimplicit-function-declaration -Wdiv-by-zero -Wunused-variable")
-
+set(CMAKE_C_FLAGS "-g -O2 -pipe -O0 -Wall -Wextra -Werror -std=gnu11 -ffreestanding -fno-stack-protector -fno-stack-check -fno-lto -fno-PIE -fno-PIC -m64 -march=x86-64 -mabi=sysv -mcmodel=kernel -mno-80387 -mno-red-zone -msse -msse2 -Wimplicit-function-declaration -Wdiv-by-zero -Wunused-variable")
 set(CMAKE_ASM_NASM_FLAGS "-felf64")
-
-# Set linker flags
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -nostdlib -static -z max-page-size=0x1000 -T linker.ld")
+set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -static -z max-page-size=0x1000 -T linker.ld")
