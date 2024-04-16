ed8664befc81e374b2216c5432303f2f9a61bb7f
Kevin Alavik <kevin@alavik.se>
2024-04-16 20:56:01 +0200

    Update x86_64-limine.toolchain.cmake

--- arch/x86_64/x86_64-limine.toolchain.cmake
+++ arch/x86_64/x86_64-limine.toolchain.cmake
@@ -16,4 +16,8 @@ endif()
 set(CMAKE_C_FLAGS "-g -O2 -pipe -O0 -Wall -Wextra -Werror -std=gnu11 -ffreestanding -fno-stack-protector -fno-stack-check -fno-lto -fno-PIE -fno-PIC -m64 -march=x86-64 -mabi=sysv -mcmodel=kernel -mno-80387 -mno-red-zone -msse -msse2 -Wimplicit-function-declaration -Wdiv-by-zero -Wunused-variable")
 set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -static -z max-page-size=0x1000 -T ${CMAKE_SOURCE_DIR}/arch/${TARGET_ARCH}/linker.ld")
 set(TARGET_BOOTLOADER "limine")
-set(TARGET_VERSION "v0.0.1")
+set(TARGET_VER "v0.0.1")
+set(TARGET_DEFINITIONS
+		LEAF_LIMINE
+    LEAF_X64
+)
