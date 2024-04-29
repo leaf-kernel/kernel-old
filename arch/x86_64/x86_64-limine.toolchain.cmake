47d686b456aa3065f68bc9e40bfe8c27f5c94014
Kevin Alavik <kevin@alavik.se>
2024-04-29 08:36:51 +0000

    fix: Fixed toolchain

--- arch/x86_64/x86_64-limine.toolchain.cmake
+++ arch/x86_64/x86_64-limine.toolchain.cmake
@@ -9,12 +9,12 @@ set(CMAKE_LINKER  x86_64-elf-ld)
 set(CMAKE_ASM_NASM_COMPILER nasm)
 
 if(CMAKE_ASM_NASM_COMPILER)
-    set(CMAKE_ASM_NASM_FLAGS "-f elf64")
     set(CMAKE_ASM_NASM_OBJECT_FORMAT "elf64")
 else()
-    message(FATAL_ERROR "NASM compiler not found!")
+    message(FATAL_ERROR "NASM not found!")
 endif()
 
+message(">>> Setting flags")
 set(COMMON_FLAGS "-std=c99 -g -ffreestanding -fno-stack-protector -fno-stack-check -fno-omit-frame-pointer -fno-lto -fPIE -m64 -march=x86-64 -mabi=sysv -mno-80387 -mno-mmx -msse -msse2 -mno-red-zone -MP")
 
 set(CMAKE_C_FLAGS "${COMMON_FLAGS} -std=gnu11")
@@ -22,9 +22,11 @@ set(CMAKE_CXX_FLAGS "${COMMON_FLAGS} -std=gnu++11")
 
 set(CMAKE_EXE_LINKER_FLAGS "-nostdlib -static -z max-page-size=0x1000 -T ${CMAKE_SOURCE_DIR}/arch/${TARGET_ARCH}/linker.ld")
 
+message(">>> Setting target bootloader and version")
 set(TARGET_BOOTLOADER "limine")
 set(TARGET_VERSION "v0.0.1")
 
+message(">>> Setting target definitions")
 set(TARGET_DEFINITIONS
     LEAF_LIMINE
     LEAF_X64
