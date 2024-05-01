fb9222969ecda3fe9cd3d1cd7e509c43e90b02ab
Kevin Alavik <kevin@alavik.se>
2024-05-01 20:48:11 +0000

    fix: Fixed install script

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,4 +1,1 @@
-sudo apt update
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin/"; brew install wget unzip cmake ninja xorriso nasm x86_64-elf-gcc x86_64-elf-binutils
-sudo apt install qemu-system clang-format
+export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin/"; sudo apt update; /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; brew install wget unzip cmake ninja xorriso nasm x86_64-elf-gcc x86_64-elf-binutils; sudo apt install qemu-system clang-format
