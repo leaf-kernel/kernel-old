958508444867baa0c1738e073d2719fb1811bf6f
Kevin Alavik <kevin@alavik.se>
2024-05-02 07:48:24 +0000

    feat: Started on VMM mapping

--- kernel/libc/stdlib/memory/vmm.c
+++ kernel/libc/stdlib/memory/vmm.c
@@ -23,7 +23,12 @@ struct Level1Group {
 struct Level4Group *PML4Array;
 
 void _x86_64_vmm_map(uint64_t vaddr, uint64_t paddr, uint32_t flags) {
-    vvcdlog("Mapped 0x%.16llx -> 0x%.16llx", vaddr, paddr);
+    const uint16_t pt = (uint16_t)((vaddr & 0x0000001FF000) >> 12);
+    const uint16_t pd = (uint16_t)((vaddr & 0x00003FE00000) >> 21);
+    const uint16_t pdptr = (uint16_t)((vaddr & 0x007FC0000000) >> 30);
+    const uint16_t pml4 = (uint16_t)((vaddr & 0xFF8000000000) >> 39);
+    cdlog("PT: 0x%.4llx PD: 0x%.4llx PDPTR: 0x%.4llx PML4: 0x%.4llx ", pt, pd,
+          pdptr, pml4);
 }
 
 void init_vmm() { cdlog("done."); }
