e34f65282c015f5ea5282db62ed1b4f34e19ca2a
Kevin Alavik <kevin@alavik.se>
2024-05-04 00:30:28 +0200

    fix: Fixed hash

--- kernel/utils/hash.c
+++ kernel/utils/hash.c
@@ -6,20 +6,12 @@
 
 uint32_t hash_string(const char *in) {
     char *str = (char *)in;
-    uint32_t hash = 0;
-    uint32_t magic1 = 0x1b2c3d4e;
-    uint32_t magic2 = 0x5f6a7b8c;
-    uint32_t offset = (uint32_t)((atoi(LEAF_OFFSET) ^ magic1 / magic2));
+    uint32_t hash = 5381;
+    int c;
 
-    while(*str != '\0') {
-        uint32_t i = *str;
-        hash += i * offset;
-        str++;
+    while((c = *str++)) {
+        hash = ((hash << 5) + hash) + c;
     }
-
-    hash += magic1 << hash;
-    hash += hash >> magic2;
-    hash ^= offset;
 
     return hash;
 }
