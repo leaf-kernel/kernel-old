93cdc73fba66cb8f3cdc68d758edfac4afd14591
Kevin Alavik <kevin@alavik.se>
2024-05-03 05:24:42 +0000

    feat: Added PCI

---
+++ kernel/include/drivers/pci/pci.h
@@ -0,0 +1,35 @@
+#ifndef __PCI_H__
+#define __PCI_H__
+
+#include <drivers/serial/serial.h>
+#define LEAF_INCLUDE_PRIVATE
+#include <stdint.h>
+#include <sys/leaf.h>
+
+typedef struct device_descriptor {
+    uint32_t portBase;
+    uint32_t interrupt;
+
+    uint16_t bus;
+    uint16_t device;
+    uint16_t function;
+
+    uint16_t vendor_id;
+    uint16_t device_id;
+
+    uint8_t class_id;
+    uint8_t subclass_id;
+    uint8_t interface_id;
+
+    uint8_t revision;
+} device_descriptor;
+
+uint32_t read_pci(uint16_t bus, uint16_t device, uint16_t function,
+                  uint32_t regoffset);
+void write_pci(uint16_t bus, uint16_t device, uint16_t function,
+               uint32_t regoffset, uint32_t data);
+void init_pci();
+device_descriptor get_device_descriptor(uint16_t bus, uint16_t device,
+                                        uint16_t function);
+
+#endif  // __PCI_H__
--- kernel/leaf.c
+++ kernel/leaf.c
@@ -4,6 +4,7 @@
 #include <arch/cpu/cpu.h>
 #include <arch/cpu/cpuid.h>
 #include <arch/cpu/utils.h>
+#include <drivers/pci/pci.h>
 #include <fs/vfs.h>
 #include <libc/stdlib/memory/kheap.h>
 #include <sys/_config.h>
@@ -20,6 +21,8 @@ int main() {
     printf("%.3s %s %d %d:%d:%d 20%d\r\n", _get_day(time.day_of_week),
            _get_month(time.month), time.day_of_month, time.hours, time.minutes,
            time.seconds, time.year);
+
+    init_pci();
 
     hlt();
     return LEAF_RETURN_SUCCESS;
