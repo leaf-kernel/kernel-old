0fe236291b67a89544680fca278094f4dc6f321d
Kevin Alavik <kevin@alavik.se>
2024-05-04 22:31:27 +0200

    feat: Added more includes in port-kinit

--- kernel/leaf.c
+++ kernel/leaf.c
@@ -5,9 +5,11 @@
 #include <arch/cpu/cpuid.h>
 #include <arch/cpu/utils.h>
 #include <arch/x86_64/acpi/mcfg.h>
+#include <arch/x86_64/idt/idt.h>
 #include <fs/vfs.h>
 #include <libc/stdlib/memory/kheap.h>
 #include <sys/_config.h>
+#include <sys/backtrace.h>
 #include <sys/time/rtc.h>
 #include <utils/parsing/ini.h>
 
