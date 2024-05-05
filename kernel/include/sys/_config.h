fbb3d01afe4aa029ecb892d4f6026d6c06bed2e2
Kevin Alavik <kevin@alavik.se>
2024-05-05 15:34:12 +0200

    feat: Disabled VERBOSE by default

--- kernel/include/sys/_config.h
+++ kernel/include/sys/_config.h
@@ -1,7 +1,7 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define __LEAF_VERBOSE__
+// #define __LEAF_VERBOSE__
 // #define __LEAF_VVERBOSE__
 
 // Extra-Extra verbose logging is only for the symbol table atm.
